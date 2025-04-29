ca46cbf09d5fb283fcfd520f2cc35ac5cbdf8667
rmc8 <k@rmc-8.com>
2025-04-29 20:23:56 +0900

    Adds length validation for image URLs
    Ensures image URLs do not exceed the maximum allowed length to prevent webhook send failures.
    Adds a constant for the maximum length allowed.

--- tech_feeds_digest/discord.py
+++ tech_feeds_digest/discord.py
@@ -9,6 +9,8 @@ class Discord:
     A class responsible for sending messages to a Discord channel via webhook.
     """
 
+    MAX_LENGTH = 1028
+
     def __init__(self, config: DiscordConfig):
         """
         Initializes the Discord client with the provided configuration.
@@ -37,7 +39,7 @@ class Discord:
             embed.set_author(name=message["author"])
             embed.add_field(name="Tags", value=", ".join(message["tags"]), inline=False)
             image_url: str | None = message["image_url"]
-            if isinstance(image_url, str):
+            if isinstance(image_url, str) and len(image_url) <= self.MAX_LENGTH:
                 embed.set_image(url=image_url)
             try:
                 await webhook.send(embed=embed)
